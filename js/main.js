e8e9afb4924900e03bef114a7f52b9c748656749
Josue Vasquez <by.jv12@gmail.com>
2022-03-03 23:02:45 -0300

    Desafio DOM

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
-const fichas = ["X", "O"];
-let incio = confirm("Desea empezar el juego?");
-let estadoDeLaPartida = true
-let puntuacion = () => {
-    if(estadoDeLaPartida === true) {
-        for (let i = 0; i < 100; i +=5) {
-            let score = i;
-            console.log(score);
-        }
+function winCase() {
+    const div = document.querySelector('main > div');
+    div.className = "end-match";
+
+    const span = document.createElement('span');
+    span.innerText = "You Win";
+
+    const button = document.createElement('button');
+    button.innerText = "Restart";
+
+    div.prepend(span)
+    div.appendChild(button)
+}
+
+function lostCase() {
+    const div = document.querySelector('main > div');
+    div.className = "end-match";
+
+    const span = document.createElement('span');
+    span.innerText = "You Lost";
+
+    const button = document.createElement('button');
+    button.innerText = "Restart";
+
+    div.prepend(span)
+    div.appendChild(button)
+}
+
+
+const funcion = (a) => {
+    a = Math.round(Math.random() * 10);
+    if (a % 2 === 0) {
+        winCase()
+
+        const box = document.querySelector('.box');
+        box.className = "blur";
+
+    } else {
+        lostCase()
+        
+        const box = document.querySelector('.box');
+        box.className = "blur";
     }
-};
-puntuacion()
+}
+funcion()
